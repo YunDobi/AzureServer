035ef1536eab0123b431be3edc09322b6edfdced
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-02-19 04:29:35 -0800

    get and post method

--- routes/add.js
+++ routes/add.js
@@ -2,33 +2,8 @@ let express = require('express');
 let router = express.Router();
 const sql = require('mssql');
 
-router.get('/', function (req, res) {
-  let column = ['name', 'email', 'passowrd'];
-  let VALUES = ['Alice', 'email1@mail.com', '123'];
-  const config = {
-    user: process.env.user, // better stored in an app setting such as process.env.DB_USER
-    password: process.env.password, // better stored in an app setting such as process.env.DB_PASSWORD
-    server: process.env.server, // better stored in an app setting such as process.env.DB_SERVER
-    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-    database: process.env.database, // better stored in an app setting such as process.env.DB_NAME
-    authentication: {
-      type: 'default',
-    },
-    options: {
-      encrypt: true,
-    },
-  };
-  console.log('Starting...');
-  connectAndQuery();
-
-  // eslint-disable-next-line func-style
-  async function connectAndQuery() {
-    await sql.connect(config, async (err) => {
-      // ... error checks
-      const result = await sql.query`insert into clients (name, email, passowrd) VALUEs (${VALUES[0]}, ${VALUES[1]}, ${VALUES[2]});`;
-      console.log(result.rowsAffected[0]);
-      res.render('add', {clent: result.rowsAffected});
-    });
-  }
+router.get('/', function(req, res) {
+  
+  res.render('add', {title: "title"});
 });
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,51 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
-
-
 let express = require('express');
 let router = express.Router();
 const sql = require('mssql');
 require("dotenv").config();
 
+const config = {
+  user: process.env.user, // better stored in an app setting such as process.env.DB_USER
+  password: process.env.password, // better stored in an app setting such as process.env.DB_PASSWORD
+  server: process.env.server, // better stored in an app setting such as process.env.DB_SERVER
+  port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+  database: process.env.database, // better stored in an app setting such as process.env.DB_NAME
+  authentication: {
+    type: 'default',
+  },
+  options: {
+    encrypt: true,
+  },
+};
+
+// eslint-disable-next-line func-style
+async function connectAndQuery(req, res) {
+  try {
+    let poolConnection = await sql.connect(config);
+
+    console.log('Reading rows from the Table...');
+    let resultSet = await poolConnection
+      .request()
+      .query(`SELECT * from [dbo]. [clients];`);
+
+    console.log(`${resultSet.recordset.length} rows returned.`);
+    console.log(resultSet.recordset);
+    res.render('index',{clents: resultSet.recordset});
+
+    poolConnection.close();
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('Starting...');
+  connectAndQuery(req, res);
+
+});
+
+router.post('/', function (req, res) {
+  let VALUES = [req.body.name, req.body.email, req.body.password];
   const config = {
     user: process.env.user, // better stored in an app setting such as process.env.DB_USER
     password: process.env.password, // better stored in an app setting such as process.env.DB_PASSWORD
@@ -30,26 +60,16 @@ router.get('/', function(req, res, next) {
     },
   };
   console.log('Starting...');
-  connectAndQuery();
+  InsertAndQuery();
+  connectAndQuery(req, res);
 
   // eslint-disable-next-line func-style
-  async function connectAndQuery() {
-    try {
-      let poolConnection = await sql.connect(config);
-
-      console.log('Reading rows from the Table...');
-      let resultSet = await poolConnection
-        .request()
-        .query(`SELECT * from [dbo]. [clients];`);
-
-      console.log(`${resultSet.recordset.length} rows returned.`);
-      console.log(resultSet.recordset);
-      res.render('index',{clents: resultSet.recordset});
-
-      poolConnection.close();
-    } catch (err) {
-      console.error(err.message);
-    }
+  async function InsertAndQuery() {
+    await sql.connect(config, async (err) => {
+      // ... error checks
+      const result = await sql.query`insert into clients (name, email, passowrd) VALUEs (${VALUES[0]}, ${VALUES[1]}, ${VALUES[2]});`;
+      console.log(result.rowsAffected[0]);
+    });
   }
 });
 
